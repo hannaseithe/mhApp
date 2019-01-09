30c1b5e9c01f6fada3829017acdb8fc73930688b
Hanna <hannaseithe@googlemail.com>
2019-01-09 15:57:53 +0100

    Zwischenstand Chart component

--- src/components/session-chart/session-chart.ts
+++ src/components/session-chart/session-chart.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Chart } from 'chart.js';
 import { FearStep } from '../../models/fearStep.model';
+import { Platform } from 'ionic-angular';
 
 /**
  * Generated class for the SessionChartComponent component.
@@ -18,10 +19,65 @@ export class SessionChartComponent {
   chart: any[] = [];
   fearSteps: FearStep[];
 
-  constructor() {
+  constructor(private platform: Platform) {
+    console.log('Constructor SessionStartComponent')
+    this.platform.ready()
+    .then(() => {
+      console.log('device ready');
+      try {
+        setTimeout(this.renderChart,1000);
+      }
+      catch(error) {
+        console.error(error)
+      }
+    })
   }
 
-  ngAfterViewInit() {
+  renderChart() {
+    console.log('renderChart()');
+    try{
+      this.chart = new Chart('myCanvas', {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+    }
+    catch(error) {
+      console.log(error)
+    }
+    
+    console.log('this.chart', this.chart);
     /*   if (this.data) {
         let datasets = [];
         this.data.forEach((fearStep) => {
@@ -46,43 +102,7 @@ export class SessionChartComponent {
           borderColor: "#112233",
           fill: false
         }] */
-        var myChart = new Chart(document.getElementById("myCanvas"), {
-          type: 'bar',
-          data: {
-              labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-              datasets: [{
-                  label: '# of Votes',
-                  data: [12, 19, 3, 5, 2, 3],
-                  backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)'
-                  ],
-                  borderColor: [
-                      'rgba(255,99,132,1)',
-                      'rgba(54, 162, 235, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 159, 64, 1)'
-                  ],
-                  borderWidth: 1
-              }]
-          },
-          options: {
-              scales: {
-                  yAxes: [{
-                      ticks: {
-                          beginAtZero:true
-                      }
-                  }]
-              }
-          }
-      });
- console.log(this.chart);
+
   }
 
   /*  } */
