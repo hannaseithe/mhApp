6ac8495818efa71b0f4b2fe4696eb08b79706458
Hanna <hannaseithe@googlemail.com>
2019-01-20 15:25:42 +0100

    Zwischenstand Animation

--- src/components/animation/animation.ts
+++ src/components/animation/animation.ts
@@ -54,16 +54,18 @@ export class AnimationComponent {
   initialiseWebGLObjectAndEnvironment(): void {
     this._ELEMENT = this.canvasEl.nativeElement;
     this._SCENE = new THREE.Scene();
-    this._SCENE.background = new THREE.TextureLoader().load("../../assets/imgs/bamboo.jpg");
+    this._SCENE.background = new THREE.CubeTextureLoader()
+    .setPath( '../../assets/imgs/cubeMap/' )
+    .load( [ 'left.jpg', 'right.jpg', 'oben.jpg', 'unten.jpg', 'vorne.jpg', 'hinten.jpg' ] );
     this._CAMERA = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this._CAMERA.position.z = 320;
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this._ELEMENT.appendChild(this.renderer.domElement);
     this._GEOMETRY = new THREE.SphereGeometry(10, 32, 16);
-    this._MATERIAL = new THREE.MeshBasicMaterial({ color: 0xffffff, envMap: this._SCENE.background, refractionRatio: 0.95 });
+    this._MATERIAL = new THREE.MeshBasicMaterial({ color: 0xffffaa, envMap: this._SCENE.background, refractionRatio: 0.75 });
     this._MATERIAL.envMap.mapping = THREE.CubeRefractionMapping;
-    for (var i = 0; i < 500; i++) {
+    for (var i = 0; i < 50; i++) {
       var mesh = new THREE.Mesh(this._GEOMETRY, this._MATERIAL);
       mesh.position.x = Math.random() * 1000 - 500;
       mesh.position.y = Math.random() * 1000 - 500;
@@ -75,34 +77,6 @@ export class AnimationComponent {
   }
 
 
-  /*     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
-      camera.position.z = 3200;
-      scene = new THREE.Scene();
-      scene.background = new THREE.CubeTextureLoader()
-        .setPath( 'textures/cube/Park3Med/' )
-        .load( [ 'px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg' ] );
-      var geometry = new THREE.SphereBufferGeometry( 100, 32, 16 );
-      var material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: scene.background, refractionRatio: 0.95 } );
-      material.envMap.mapping = THREE.CubeRefractionMapping;
-      for ( var i = 0; i < 500; i ++ ) {
-        var mesh = new THREE.Mesh( geometry, material );
-        mesh.position.x = Math.random() * 10000 - 5000;
-        mesh.position.y = Math.random() * 10000 - 5000;
-        mesh.position.z = Math.random() * 10000 - 5000;
-        mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
-        scene.add( mesh );
-        spheres.push( mesh );
-      }
-      //
-      renderer = new THREE.WebGLRenderer();
-      renderer.setPixelRatio( window.devicePixelRatio );
-      renderer.setSize( window.innerWidth, window.innerHeight );
-      container.appendChild( renderer.domElement );
-      //
-      window.addEventListener( 'resize', onWindowResize, false );
-    } */
-
-
   private _animate(): void {
     requestAnimationFrame(() => {
       this._animate();
@@ -111,8 +85,9 @@ export class AnimationComponent {
     var timer = 0.0001 * Date.now();
     for (var i = 0, il = this.spheres.length; i < il; i++) {
       var sphere = this.spheres[i];
-      sphere.position.x = 5000 * Math.cos(timer + i);
-      sphere.position.y = 5000 * Math.sin(timer + i * 1.1);
+      sphere.position.x = 200 * Math.cos(timer + i);
+      sphere.position.y = 200 * Math.sin(timer + i * 1.1);
+      sphere.position.z = 200 * Math.sin(timer + i * 1.1);
     }
 
     this.renderer.render(this._SCENE, this._CAMERA);
